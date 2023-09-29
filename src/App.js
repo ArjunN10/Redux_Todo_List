c20d4aaf37d467f998ee133c99f036f3714413ae
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-09-29 19:03:52 +0530

    redux_todo

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
 import { useSelector, useDispatch } from "react-redux";
-import { addtodo ,deletetodo,edittodo,savetodo } from "./components/Todo";
-
-// import "./App.css";
-import { useRef } from "react";
+import { addtodo, deletetodo, edittodo, savetodo } from "./components/Todo";
+import { useRef, useState } from "react";
 
 function App() {
-  const myRef=useRef()
+  const myRef = useRef();
   const todo = useSelector((state) => state.list.todos);
-  // console.log(todo);
-
   const dispatch = useDispatch();
+  const [editValue, setEditValue] = useState("");
 
   const add = (e) => {
     e.preventDefault();
     const value = e.target.todo.value;
     dispatch(addtodo(value));
-    e.target.reset()
+    e.target.reset();
   };
 
-const save = (id)=>{
-  const savevalue = myRef.current.value
-  dispatch(savetodo({id:id,value:savevalue}))
-}
+  const save = (id) => {
+    const saveValue = myRef.current.value;
+    dispatch(savetodo({ id: id, value: saveValue }));
+  };
 
+  const handleEditClick = (id, value) => {
+    // When the "Edit" button is clicked, set the edited value in the state
+    setEditValue(value);
+    dispatch(edittodo(id)); // Dispatch the edit action
+  };
 
   return (
     <div className="App">
       <h1>TODO APP</h1>
       <form onSubmit={add}>
-        <input placeholder="Add here.." id="todo" type="text"  />
+        <input placeholder="Add here.." id="todo" type="text" />
         <button type="submit">ADD</button>
       </form>
 
       <ul>
         {todo.map((todos) => (
-          <li>
-
-          {(todos.editkey==true)?
-          <>
-            {todos.value}
-            <button className="delete" onClick={()=>dispatch(deletetodo(todos.id))}>Delete</button>
-            <button className="edit" onClick={()=>dispatch(edittodo(todos.id))}>edit</button>
-            </>:
-            <>
-              <input type="text" ref={myRef} value={todos.value} /> <button type="button" onChange={()=>save(todos.id)}>save</button>
-            </>
-          }</li>
+          <li key={todos.id}>
+            {todos.editkey ? (
+              <>
+                <span>{todos.value}</span>
+                <button className="delete" onClick={() => dispatch(deletetodo(todos.id))}>
+                  Delete
+                </button>
+              </>
+            ) : (
+              <>
+                <input type="text" ref={myRef} value={editValue} onChange={(e) => setEditValue(e.target.value)} />
+                <button type="button" onClick={() => save(todos.id)}>Save</button>
+              </>
+            )}
+            <button className="edit" onClick={() => handleEditClick(todos.id, todos.value)}>Edit</button>
+          </li>
         ))}
       </ul>
     </div>
