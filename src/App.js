1daa2f04105ca9c6922020381dd555f8aa1e0986
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-09-30 06:02:36 +0530

    redux_todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addtodo, deletetodo, edittodo, savetodo } from "./components/Todo";
 import { useRef, useState } from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+} from "mdb-react-ui-kit";
 
 function App() {
   const myRef = useRef();
@@ -27,33 +36,83 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>TODO APP</h1>
-      <form onSubmit={add}>
-        <input placeholder="Add here.." id="todo" type="text" />
-        <button type="submit">ADD</button>
-      </form>
-
-      <ul>
-        {todo.map((todos) => (
-          <li key={todos.id}>
-            {todos.editkey ? (
-              <>
-                <span>{todos.value}</span>
-                <button className="delete" onClick={() => dispatch(deletetodo(todos.id))}>
-                  Delete
-                </button>
-              </>
-            ) : (
-              <>
-                <input type="text" ref={myRef} value={editValue} onChange={(e) => setEditValue(e.target.value)} />
-                <button type="button" onClick={() => save(todos.id)}>Save</button>
-              </>
-            )}
-            <button className="edit" onClick={() => handleEditClick(todos.id, todos.value)}>Edit</button>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <MDBContainer className="py-5">
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol>
+            <MDBCard
+              style={{ borderRadius: ".75rem", backgroundColor: "#eff1f2" }}
+              >
+              <MDBCardBody className="py-4 px-4 px-md-5">
+                <p className="h1 text-center mt-3 mb-4 pb-3 text-primary">
+                  <MDBIcon fas icon="check-square" className="me-1" />
+                  <u>Todo-App</u>
+                </p>
+                <div className="pb-2">
+                  <form onSubmit={add}>
+                    <MDBCard className=" mx-auto sm-12 lg-2 md-3" style={{maxWidth:'700px',maxHeight:'100vh'}}>
+                      <MDBCardBody>
+                        <div className="d-flex flex-row align-items-center">
+                          <input
+                            type="text"
+                            className="form-control form-control-lg"
+                            placeholder="Add new..."
+                            id="todo"
+                          />
+                          <div className="mx-2">
+                            <MDBBtn>Add</MDBBtn>
+                          </div>
+                        </div>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </form>
+                  
+                  <ul type="square" >
+                    {todo.map((todos) => (
+                      <li className="  my-3" key={todos.id}>
+                        {todos.editkey === true ? (
+                          <>
+                            <span>{todos.value}</span>
+                            <MDBBtn
+                              className="delete mx-3 bg-danger"
+                              onClick={() => dispatch(deletetodo(todos.id))}
+                            >
+                              Delete
+                            </MDBBtn>
+                          </>
+                        ) : (
+                          <>
+                            <input
+                              type="text"
+                              className="form-control form-control-lg w-50"
+                              ref={myRef}
+                              value={editValue}
+                              onChange={(e) => setEditValue(e.target.value)}
+                            />
+                            <MDBBtn
+                              className="mx-2 bg-success"
+                              type="button"
+                              onClick={() => save(todos.id)}
+                            >
+                              Save
+                            </MDBBtn>
+                          </>
+                        )}
+                        <MDBBtn
+                          className="edit mx-2 bg-dark"
+                          onClick={() => handleEditClick(todos.id, todos.value)}
+                        >
+                          Edit
+                        </MDBBtn>
+                      </li>
+                    ))}
+                  </ul>
+                  </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Provider } from "react-redux";
 import { store } from "./components/Store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
