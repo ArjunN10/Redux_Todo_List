b18d0b51b6c6fd3a9391515d6e971ea4872800e5
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-10-14 14:51:57 +0530

    todo_app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addtodo, deletetodo, edittodo, savetodo } from "./components/Todo";
-import {   useRef } from "react";
+import { useRef } from "react";
 import {
   MDBBtn,
   MDBCard,
@@ -28,8 +28,6 @@ function App() {
     dispatch(savetodo({ id: id, value: saveValue }));
   };
 
-
-
   return (
     <div>
       <MDBContainer className="py-5">
@@ -37,15 +35,23 @@ function App() {
           <MDBCol>
             <MDBCard
               style={{ borderRadius: ".75rem", backgroundColor: "#352F44" }}
-              >
+            >
               <MDBCardBody className="py-4 px-4 px-md-5">
                 <p className="h1 text-center mt-3 mb-4 pb-3 text-primary">
-                  <MDBIcon fas icon="check-square" color='success' className="me-1" />
+                  <MDBIcon
+                    fas
+                    icon="check-square"
+                    color="success"
+                    className="me-1"
+                  />
                   <u>Todo-App</u>
                 </p>
                 <div className="pb-2">
                   <form onSubmit={add}>
-                    <MDBCard className=" mx-auto sm-12 lg-2 md-3" style={{maxWidth:'700px',maxHeight:'100vh'}}>
+                    <MDBCard
+                      className=" mx-auto sm-12 lg-2 md-3"
+                      style={{ maxWidth: "700px", maxHeight: "100vh" }}
+                    >
                       <MDBCardBody>
                         <div className="d-flex flex-row align-items-center">
                           <input
@@ -63,53 +69,51 @@ function App() {
                     </MDBCard>
                   </form>
 
-
-                  <ol className="mt-5 text-white "  style={{fontSize:'20px'}}>
+                  <ol className="mt-5 text-white " style={{ fontSize: "20px" }}>
                     {todo.map((todos) => (
                       <li className="  my-3 " key={todos.id}>
                         {todos.editkey === true ? (
                           <>
-                              <input
+                            <input
                               type="text"
-                              className="form-control form-control-lg sm-12 lg-2 md-3" style={{maxWidth:'400px',maxHeight:'100vh'}}
+                              className="form-control form-control-lg sm-12 lg-2 md-3"
+                              style={{ maxWidth: "400px", maxHeight: "100vh" }}
                               ref={myRef}
                               value={todos.value}
                               required
-                               />
+                            />
                             <MDBBtn
                               className="delete mx-3 mt-3 bg-danger"
                               onClick={() => dispatch(deletetodo(todos.id))}
-
                             >
                               Delete
-                            <MDBIcon
-                            fas
-                            icon="trash-alt"
-                            color="white"
-                            size="lg"
-                            className="ms-1"
-                          />
+                              <MDBIcon
+                                fas
+                                icon="trash-alt"
+                                color="white"
+                                size="lg"
+                                className="ms-1"
+                              />
                             </MDBBtn>
                             <MDBBtn
-                          className="edit mx-2 mt-3 bg-dark"
-                          onClick={() =>dispatch(edittodo(todos.id))
-                          
-                          }
-                        >
-                          <MDBIcon
-                        fas
-                        icon="pencil-alt"
-                        className="me-1"
-                        color="info"
-                        />
-                        Edit
-                        </MDBBtn>
+                              className="edit mx-2 mt-3 bg-dark"
+                              onClick={() => dispatch(edittodo(todos.id))}
+                            >
+                              <MDBIcon
+                                fas
+                                icon="pencil-alt"
+                                className="me-1"
+                                color="info"
+                              />
+                              Edit
+                            </MDBBtn>
                           </>
                         ) : (
                           <>
                             <input
                               type="text"
-                              className="form-control form-control-lg sm-12 lg-2 md-3" style={{maxWidth:'400px',maxHeight:'100vh'}}
+                              className="form-control form-control-lg sm-12 lg-2 md-3"
+                              style={{ maxWidth: "400px", maxHeight: "100vh" }}
                               ref={myRef}
                               required
                             />
@@ -120,17 +124,18 @@ function App() {
                             >
                               Save
                               <MDBIcon
-                               fas icon="check"
-                               className="ms-1"
-                               alt='icon' />
+                                fas
+                                icon="check"
+                                className="ms-1"
+                                alt="icon"
+                              />
                             </MDBBtn>
                           </>
                         )}
-                       
                       </li>
                     ))}
                   </ol>
-                  </div>
+                </div>
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
